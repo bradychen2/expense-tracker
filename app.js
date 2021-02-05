adeba8ab6cea97ec672b7abc2d153c4d3243ad2f
bradychen <bradychen2@gmail.com>
2021-02-05 16:17:03 +0800

    feat: project init
    Set up project structure: views, models
    Create app.js
    Setup necessary packsge

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+require('./config/mongoose')
+
+const app = express()
+const port = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on 'http://localhost:${port}'`)
+})
---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/expense-tracker')
+
+const db = mongoose.connection
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+module.exports = db
