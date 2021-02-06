bdd6b8b5d7c1110a9004932343d75c6c83ead7f9
bradychen <bradychen2@gmail.com>
2021-02-06 23:29:13 +0800

    feat: edit function & modified index
    Create edit function
    Add handlebarsHelper judgeCategory
    Modified index dropdown list

--- app.js
+++ app.js
@@ -22,14 +22,32 @@ app.get('/', (req, res) => {
   return Record.find()
     .lean()
     .sort({ date: 'desc' })
-    .then(records => {
-      res.render('index', { records })
-    })
+    .then(records => res.render('index', { records }))
     .catch(error => console.log(error))
 })
 
 
-app.delete('/:id', (req, res) => {
+app.get('/records/edit/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then(record => res.render('edit', { record }))
+    .catch(error => { console.log(error) })
+})
+
+app.put('/records/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then((record) => {
+      record = Object.assign(record, req.body)
+      return record.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+
+app.delete('/records/:id', (req, res) => {
   const id = req.params.id
   return Record.findById(id)
     .then(record => record.remove())
--- views/handlebarsHelpers.js
+++ views/handlebarsHelpers.js
@@ -1,9 +1,19 @@
 const Handlebars = require("handlebars")
 
 Handlebars.registerHelper('formatTime', function (dayTime) {
+  // Need transform type when retrieve from mongodb
   let recordDate = new Date(dayTime)
-  const year = recordDate.getFullYear()
-  const month = recordDate.getMonth() + 1
-  const date = recordDate.getDate()
+  let year = recordDate.getFullYear()
+  let month = recordDate.getMonth() + 1
+  let date = recordDate.getDate()
+  if (month < 10) { month = '0' + month }
+  if (date < 10) { date = '0' + date }
   return year + '-' + month + '-' + date
+})
+
+Handlebars.registerHelper('judgeCategory', function (recordCategory, currCategory) {
+  if (recordCategory === currCategory) {
+    return 'selected'
+  }
+  return
 })
