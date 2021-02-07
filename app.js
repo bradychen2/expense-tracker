4e218a91f2608415f6436580665015c8d82ed9f5
bradychen <bradychen2@gmail.com>
2021-02-07 10:31:40 +0800

    feat: create add new func
    Create add new expense function and new.hbs

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-
+// Home
 app.get('/', (req, res) => {
   return Record.find()
     .lean()
@@ -26,7 +26,25 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// Go to create page
+app.get('/records/new', (req, res) => {
+  res.render('new')
+})
 
+// Create new expense record
+app.post('/records', (req, res) => {
+  const record = req.body
+  return Record.create(record)
+    .then(() => res.redirect('/'))
+    .catch(error => {
+      console.log(error)
+      if (error._message === 'Record validation failed') {
+        res.render('new', { record, error })
+      }
+    })
+})
+
+// Go to edit page
 app.get('/records/edit/:id', (req, res) => {
   const id = req.params.id
   return Record.findById(id)
@@ -35,10 +53,11 @@ app.get('/records/edit/:id', (req, res) => {
     .catch(error => { console.log(error) })
 })
 
+// Edit record
 app.put('/records/:id', (req, res) => {
   const id = req.params.id
   return Record.findById(id)
-    .then((record) => {
+    .then(record => {
       record = Object.assign(record, req.body)
       return record.save()
     })
@@ -46,7 +65,7 @@ app.put('/records/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-
+// Delete record
 app.delete('/records/:id', (req, res) => {
   const id = req.params.id
   return Record.findById(id)
