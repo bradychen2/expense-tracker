c432c7cc6c7f2882712fdbd65e4ab52fd2c7cc46
bradychen <bradychen2@gmail.com>
2021-03-21 18:03:10 +0800

    feat: add isAuthenticated to display diff views.

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.use(session({
 }))
 usePassport(app)
 app.use(express.static('./public'))
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 
 app.listen(PORT, () => {
