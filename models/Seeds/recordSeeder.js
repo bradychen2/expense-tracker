85235d6db41b950d99a7fc0d3ea80df5174628b8
bradychen <bradychen2@gmail.com>
2021-03-25 10:52:01 +0800

    refac: modify recordSeeder

--- models/Seeds/recordSeeder.js
+++ models/Seeds/recordSeeder.js
@@ -1,23 +1,41 @@
-// require('../../config/mongoose')
-const mongoose = require('mongoose')
 const Record = require('../record')
+const User = require('../user')
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config()
+}
 const generator = require('./randomGenerator')
 
-mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
+const db = require('../../config/mongoose')
+const SEED_USER = {
+  name: 'test',
+  email: 'test@test.com',
+  password: '123456789'
+}
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-  for (let i = 0; i < 10; i++) {
-    Record.create({
-      name: `record-${i}`,
-      category: 'housing',
-      date: generator.randomDate(new Date('1900-01-01'), new Date('2100-12-31')),
-      amount: generator.randomAmount(10000)
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(async function (user) {
+      const userId = user._id
+      for (let i = 0; i < 10; i++) {
+        await Record.create({
+          name: `record-${i}`,
+          category: generator.randomCategory(),
+          date: generator.randomDate(new Date('1900-01-01'), new Date('2100-12-31')),
+          amount: generator.randomAmount(10000),
+          userId
+        }).catch(err => console.log(err))
+      }
+      console.log('done!')
+      process.exit()
     })
-  }
-  console.log('done !')
+    .catch(err => console.log(err))
 })
 
