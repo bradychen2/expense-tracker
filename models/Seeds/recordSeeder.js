19e2e280f3892e9bad41f04cb72a11941e229856
bradychen <bradychen2@gmail.com>
2021-06-04 00:45:52 +0800

    refac: change seeder to async await syntax

--- models/Seeds/recordSeeder.js
+++ models/Seeds/recordSeeder.js
@@ -13,29 +13,33 @@ const SEED_USER = {
   password: '123456789'
 }
 
-db.once('open', () => {
-  bcrypt
-    .genSalt(10)
-    .then(salt => bcrypt.hash(SEED_USER.password, salt))
-    .then(hash => User.create({
+db.once('open', async () => {
+
+  try {
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(SEED_USER.password, salt)
+    const user = await User.create({
       name: SEED_USER.name,
       email: SEED_USER.email,
       password: hash
-    }))
-    .then(async function (user) {
-      const userId = user._id
-      for (let i = 0; i < 10; i++) {
-        await Record.create({
-          name: `record-${i}`,
-          category: generator.randomCategory(),
-          date: generator.randomDate(new Date('2020-01-01'), new Date('2021-12-31')),
-          amount: generator.randomAmount(10000),
-          userId
-        }).catch(err => console.log(err))
-      }
-      console.log('done!')
-      process.exit()
     })
-    .catch(err => console.log(err))
+    const userId = user._id
+
+    for (let i = 0; i < 10; i++) {
+      await Record.create({
+        name: `record-${i}`,
+        category: generator.randomCategory(),
+        date: generator.randomDate(new Date('2020-01-01'), new Date('2021-12-31')),
+        amount: generator.randomAmount(10000),
+        userId
+      })
+    }
+
+    console.log('done!')
+    process.exit()
+
+  } catch (err) {
+    console.log(err)
+  }
 })
 
